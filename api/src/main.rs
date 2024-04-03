95b5f756cbd66515e855fa53b94efe03b838f0d8
wkiese <w.kiese@sks-kinkel.de>
2024-04-03 13:21:02 +0200

    testing api added

---
+++ api/src/main.rs
@@ -0,0 +1,32 @@
+use rocket::http::uri::Origin;
+use rocket::http::Status;
+use rocket::response::Redirect;
+use rocket::serde::json::{json, Value};
+
+#[macro_use]
+extern crate rocket;
+
+const TAURI_RELEASES_PREFIX: Origin<'static> = uri!("/tauri-releases");
+
+#[get("/")]
+fn index() -> Redirect {
+    let msg: Option<&str> = None;
+    Redirect::to(uri!(
+        TAURI_RELEASES_PREFIX,
+        google_keep_desktop_api("linux-x86_64", "v1.0.14", msg)
+    ))
+}
+//tauri-releases/google-keep-desktop/win64/1.18.0?msg=""
+#[get("/google-keep-desktop/<_platform>/<current_version>?<msg>")]
+fn google_keep_desktop_api(_platform: &str, current_version: &str, msg: Option<&str>) -> Value {
+    //Status::NoContent
+    json!({
+        "notes": "IT Works"
+    })
+}
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/tauri-releases", routes![google_keep_desktop_api])
+}
