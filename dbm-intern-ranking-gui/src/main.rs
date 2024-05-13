0b33692c5c126386d35649a647d985881dd1ba02
wkiese <w.kiese@sks-kinkel.de>
2024-05-13 11:56:33 +0200

    forced dark mode, added catpuccin theme

--- dbm-intern-ranking-gui/src/main.rs
+++ dbm-intern-ranking-gui/src/main.rs
@@ -29,29 +29,13 @@ impl DBMInternRanking {
 impl App for DBMInternRanking {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut Frame) {
         ctx.set_pixels_per_point(1.2);
-
-        if self.dark_mode {
-            ctx.set_visuals(egui::Visuals::dark());
-        } else {
-            ctx.set_visuals(egui::Visuals::light());
-        }
+        catppuccin_egui::set_theme(&ctx, catppuccin_egui::MOCHA);
 
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             ui.horizontal(|ui| {
                 ui.heading("DBMInternRanking");
 
-                ui.with_layout(
-                    egui::Layout::from_main_dir_and_cross_align(
-                        egui::Direction::RightToLeft,
-                        egui::Align::RIGHT,
-                    ),
-                    |ui| {
-                        let button_text = if self.dark_mode { "🌙" } else { "🌞" };
-                        if ui.button(button_text).clicked() {
-                            self.dark_mode = !self.dark_mode;
-                        }
-                    },
-                );
+
             });
         });
 
@@ -155,7 +139,6 @@ impl App for DBMInternRanking {
 fn main() {
     let app = DBMInternRanking::new();
     let native_options = eframe::NativeOptions {
-        initial_window_size: Some(egui::vec2(295.0, 270.0)),
         ..Default::default()
     };
     let result = eframe::run_native(
